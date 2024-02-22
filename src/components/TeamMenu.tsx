0b9d7802d21673b0a96aad74ce97b81114cb8134
Santiago Crippa <santicrippa@gmail.com>
2024-02-22 16:34:17 -0300

    adding team schedule

--- src/components/TeamMenu.tsx
+++ src/components/TeamMenu.tsx
@@ -1,14 +1,48 @@
-import React from 'react'
+'use client'
+
+import React, { useState, useEffect } from 'react';
+
+import { Mountain, mountains } from '@/data/data';
 
 export default function TeamMenu() {
+  const [selectedMountain, setSelectedMountain] = useState<Mountain>(mountains[0])
+
+  useEffect(() => {
+    // Actualizar el contenido del calendario al cambiar la montaña seleccionada
+  }, [selectedMountain]);
+
+  const handleMenuClick = (mountain: Mountain) => {
+    setSelectedMountain(mountain);
+  };
+
   return (
-    <section className='w-full bg-blue-950 h-12 z-20 relative'>
-      <ul className='flex h-full items-center'>
-        <button className='active:bg-gray-400'>
-          <li className=' text-blue-950'>MOUNTAIN 1</li>
-        </button>
-        <li>MOUNTAIN 2</li>
-      </ul>
+    <section className='relative z-20'>
+      <menu className="border border-purple-500 h-12 flex gap-5 justify-center text-lg bg-blue-950 text-slate-400 font-bold underline">
+        {mountains.map((mountain) => (
+          <button
+            className=''
+            key={mountain.name}
+            onClick={() => handleMenuClick(mountain)}>
+            {mountain.name}
+          </button>
+        ))}
+      </menu>
+
+      <section className='mt-10 w-[25rem] h-[15rem] mx-auto px-5 py-2 bg-gray-50/80 rounded-lg shadow-lg'>
+        <h3 className='text-2xl text-blue-950 font-extrabold leading-10	'>SCHEDULE</h3>
+        <div className='mt-5 transition'>
+          {selectedMountain.schedule.map((item) => (
+            <ul
+              className='grid grid-cols-2 items-center py-2 transition'
+              key={item.date}
+            >
+              <li>{item.date}</li>
+              <li>{item.description}</li>
+            </ul>
+          ))}
+        </div>
+      </section>
+
     </section>
-  )
+  );
 }
--- src/data/data.ts
+++ src/data/data.ts
@@ -23,4 +23,30 @@ export const carouselImages = [
     title: carouselSec,
     imageUrl: carouselSec
   }
-] as const
+] as const;
+
+export interface Mountain {
+  name: string;
+  schedule: { date: string; description: string }[];
+}
+
+export const mountains: Mountain[] = [
+  {
+    name: 'MOUNTAIN 1',
+    schedule: [
+      { date: '25 Nov 2016', description: 'Vestibulum viverra' },
+      { date: '28 Nov 2016', description: 'Vestibulum viverra' },
+      { date: '18 Dec 2016', description: 'Vestibulum viverra' },
+      { date: '7 Jan 2017', description: 'Vestibulum viverra' },
+    ],
+  },
+  {
+    name: 'MOUNTAIN 2',
+    schedule: [
+      { date: '10 Feb 2017', description: 'Nullam accumsan' },
+      { date: '14 Feb 2017', description: 'Maecenas tincidunt' },
+      { date: '20 Feb 2017', description: 'Proin eget tortor' },
+      { date: '25 Feb 2017', description: 'Lorem ipsum dolor' },
+    ],
+  },
+] as const;
